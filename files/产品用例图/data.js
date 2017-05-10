db975bd685927176225b6541e90f54de61627f5b
XiaoMa _ <XiaoMa _>
2017-05-10 15:28:21 +0800

    添加元件说明自定义字段

---
+++ files/产品用例图/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p],q,_(r,s,t,u,v,w,x,_(y,z),A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,M,N,M,O,P,Q,null,R,S,T,U,V,W,X,S),Y,_(),Z,_(),ba,_(bb,[])),bc,_(),bd,_());}; 
+var b="url",c="产品用例图.html",d="generationDate",e=new Date(1494401208603.15),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="defaultpage",n="nav_name",o="dropmenu",p="dropmenu_sub",q="page",r="packageId",s="0c1ed5b2a9724915a2db32584266be63",t="type",u="Axure:Page",v="name",w="产品用例图",x="notes",y="页面逻辑",z="<p><span>用例模型，使用用例的方法来描述产品需求。</span></p><p><span>用例模型是由用例图和每一个用例的详细描述文档所组成的。</span></p><p><span>用例图并不是画成了图形的用例。UML是用例图形符号最流行的标准，但是除了UML标准，用例也有其它的可选择的标准。</span></p>",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="center",F="fill",G="fillType",H="solid",I="color",J=0xFFF2F2F2,K="image",L="imageHorizontalAlignment",M="near",N="imageVerticalAlignment",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Arial",X="borderWidth",Y="adaptiveStyles",Z="interactionMap",ba="diagram",bb="objects",bc="masters",bd="objectPaths";
+return _creator();
+})());
