8c1fee35786dd349dd7c35931551a11376d7309c
XiaoMa _ <XiaoMa _>
2017-05-27 17:22:49 +0800

    YouHua

---
+++ files/业务流程图/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p],q,_(r,s,t,u,v,w,x,_(),y,_(z,A,B,C,D,_(E,F,G,H),I,null,J,K,L,K,M,N,O,null,P,Q,R,S,T,U,V,Q),W,_(X,_()),Y,_(),Z,_(ba,[])),bb,_(),bc,_());}; 
+var b="url",c="业务流程图.html",d="generationDate",e=new Date(1495876842496.12),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="defaultpage",n="nav_name",o="dropmenu",p="dropmenu_sub",q="page",r="packageId",s="e5ef4b7d774c4d0f9c6326433fcaf795",t="type",u="Axure:Page",v="name",w="业务流程图",x="notes",y="style",z="baseStyle",A="627587b6038d43cca051c114ac41ad32",B="pageAlignment",C="center",D="fill",E="fillType",F="solid",G="color",H=0xFFF2F2F2,I="image",J="imageHorizontalAlignment",K="near",L="imageVerticalAlignment",M="imageRepeat",N="auto",O="favicon",P="sketchFactor",Q="0",R="colorStyle",S="appliedColor",T="fontName",U="Arial",V="borderWidth",W="adaptiveStyles",X="a",Y="interactionMap",Z="diagram",ba="objects",bb="masters",bc="objectPaths";
+return _creator();
+})());
